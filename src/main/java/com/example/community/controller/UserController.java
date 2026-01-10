fef6b0f18d8f2198595b0426f104e45147011117
HyunWooNo <zz47911@naver.com>
2026-01-10 19:43:10 +0900

    refactor: Valid 어노테이션 추가

--- src/main/java/com/example/community/controller/UserController.java
+++ src/main/java/com/example/community/controller/UserController.java
@@ -5,11 +5,13 @@ import com.example.community.entity.User;
 import com.example.community.repository.UserRepository;
 import com.example.community.security.CustomUserDetails;
 import com.example.community.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,7 +34,11 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public String register(@ModelAttribute UserRequestDTO userRequestDTO){
+    public String register(@Valid @ModelAttribute UserRequestDTO userRequestDTO
+                            , BindingResult result){
+        if(result.hasErrors()){
+            return "register";
+        }
         userService.userRegister(userRequestDTO);
         return "redirect:/login";
     }
