e59b0fdc672579e1be16a5c9ed3111abe6417e7b
HyunWooNo <zz47911@naver.com>
2026-01-16 17:17:07 +0900

    refactor: 프로필 내가 쓴 게시물 페이지네이션 적용

--- src/main/java/com/example/community/controller/ProfileController.java
+++ src/main/java/com/example/community/controller/ProfileController.java
@@ -1,30 +1,38 @@
 package com.example.community.controller;
 
+import com.example.community.entity.Post;
 import com.example.community.entity.Profile;
 import com.example.community.entity.User;
+import com.example.community.repository.ProfileRepository;
 import com.example.community.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.security.Principal;
-
 @Controller
 @RequiredArgsConstructor
 public class ProfileController {
 
+    private final ProfileRepository profileRepository;
 
     @GetMapping("/profile")
     public String showProfile(@AuthenticationPrincipal CustomUserDetails customUserDetails
-                            , Model model){
+                            , Model model
+                            , @PageableDefault(size = 5, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable){
 
         User loggedUser = customUserDetails.getUser();
         Profile loggedUserProfile = loggedUser.getProfile();
+        Page<Post> savedMyPosts = profileRepository.findPostsByUserId(loggedUser.getUserId(), pageable);
 
         model.addAttribute("user", loggedUser);
         model.addAttribute("profile", loggedUserProfile);
+        model.addAttribute("posts", savedMyPosts);
 
         return "profile";
     }
--- src/main/java/com/example/community/repository/ProfileRepository.java
+++ src/main/java/com/example/community/repository/ProfileRepository.java
@@ -1,8 +1,14 @@
 package com.example.community.repository;
 
+import com.example.community.entity.Post;
 import com.example.community.entity.Profile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProfileRepository extends JpaRepository<Profile, Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface ProfileRepository extends JpaRepository<Profile, Long> {
+    Page<Post> findPostsByUserId(String userId, Pageable pageable);
 }
