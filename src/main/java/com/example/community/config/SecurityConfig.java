469619d8858427411f72569648415c3be023b04e
HyunWooNo <zz47911@naver.com>
2026-01-25 19:40:59 +0900

    refactor: css, jar 등 csrf 비적용으로 수정

--- src/main/java/com/example/community/config/SecurityConfig.java
+++ src/main/java/com/example/community/config/SecurityConfig.java
@@ -6,10 +6,12 @@ import com.example.community.exception.CustomAuthenticationEntryPoint;
 import com.example.community.security.MyUserDetailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -78,5 +80,12 @@ public class SecurityConfig {
                     .permitAll()
     );
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                .requestMatchers("/css/**", "/js/**", "/images/**", "/*.css", "/*.js", "/banner.jpg", "/favicon.ico"); // 직접 쓰시는 파일들
     }
 }
